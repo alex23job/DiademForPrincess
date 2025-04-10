c4d0dcfd24ce4361fec078661834f9f3555c9b69
alex23job <106354657+alex23job@users.noreply.github.com>
2025-04-10 15:25:04 +0300

    Adding generate Bonus in Way

--- TiaraForPrincess/Assets/Scripts/WayControl.cs
+++ TiaraForPrincess/Assets/Scripts/WayControl.cs
@@ -11,12 +11,14 @@ public class WayControl : MonoBehaviour
     private int[] arrColor = null;
     private GameObject[] arrRect = null;
     private GameObject[] arrPlatform = null;
+    private List<int> posBonus = null;
     // Start is called before the first frame update
     void Start()
     {
         arrColor = new int[18];
         arrRect = new GameObject[18];
         arrPlatform = new GameObject[18];
+        posBonus = new List<int>();
         GenerateWay();
     }
 
@@ -46,6 +48,25 @@ public class WayControl : MonoBehaviour
         }
     }
 
+    public void SetBonus(GameObject bonusPrefab)
+    {
+        Vector3 pos = new Vector3(0, 1f, 0);
+        List<int> candidat = new List<int>() { 3, 5, 6, 8, 9, 11, 12, 14};
+        int numPos = Random.Range(0, candidat.Count);
+        while (posBonus.Contains(candidat[numPos]))
+        {
+            candidat.RemoveAt(numPos);
+            if (candidat.Count == 0) return;
+            numPos = Random.Range(0, candidat.Count);
+        }
+        GameObject bonus = Instantiate(bonusPrefab);
+        bonus.transform.parent = transform;
+        pos.x = -4.9f + 2 * (candidat[numPos] / 3);
+        pos.z = -2f + 2 * (candidat[numPos] % 3);
+        bonus.transform.localPosition = pos;
+        posBonus.Add(candidat[numPos]);
+    }
+
     public bool GeneratePlatform(int numCol)
     {
         // список номеров €чеек с цветом из параметра функции
@@ -54,6 +75,28 @@ public class WayControl : MonoBehaviour
         for (i = 0; i < 18; i++)
         {
             if (arrColor[i] == numCol && arrPlatform[i] == null) candidat.Add(i);
+        }
+        for(i = 0; i < posBonus.Count; i++)
+        {   //  пробуем создать платформу под бонусом если р€дом уже есть платформы 
+            if (candidat.Contains(posBonus[i]))
+            {
+                numCnd = posBonus[i];
+                if ((arrPlatform[numCnd - 3] != null) || (arrPlatform[numCnd + 3] != null))   //  в предидущем или следующем р€ду есть платформа
+                {
+                    CreatePlatform(numCnd, numCol);
+                    return true;
+                }
+                if (((numCnd % 3) < 2) && (arrPlatform[numCnd + 1] != null))    //  справа есть платформа
+                {
+                    CreatePlatform(numCnd, numCol);
+                    return true;
+                }
+                if (((numCnd % 3) > 0) && (arrPlatform[numCnd - 1] != null))    //  слева есть платформа
+                {
+                    CreatePlatform(numCnd, numCol);
+                    return true;
+                }
+            }
         }
         for (i = 0; i < candidat.Count; i++)
         {
