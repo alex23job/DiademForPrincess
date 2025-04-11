dea4c717c206ddd7bdca7e1d5fe4e17ed9dff6c8
alex23job <106354657+alex23job@users.noreply.github.com>
2025-04-11 23:50:08 +0300

    Adding Menu scene and menuControl

---
+++ TiaraForPrincess/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    public void LoadTiara()
+    {
+        SceneManager.LoadScene("TiaraScene");
+    }
+}
---
+++ TiaraForPrincess/Assets/Scripts/TailPrefabPak.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailPrefabPak : MonoBehaviour
+{
+    public static TailPrefabPak Instance;
+
+    [SerializeField] private GameObject[] arrStones;
+    [SerializeField] private GameObject[] arrGoldTiles;
+
+    public int CountStones { get { return arrStones.Length; } }
+    public int CountTiles {  get { return arrGoldTiles.Length; } }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public GameObject GetStone(int index)
+    {
+        if (index >= 0 && index < arrStones.Length) return arrStones[index];
+        return null;
+    }
+
+    public GameObject GetTail(int index)
+    {
+        if (index >= 0 && index < arrGoldTiles.Length) return arrGoldTiles[index];
+        return null;
+    }
+}
