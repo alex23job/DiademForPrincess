74b87673127ccfca1d0ddc19ee1cdd2f6069e048
alex23job <106354657+alex23job@users.noreply.github.com>
2025-04-12 01:39:57 +0300

    Adding menu button in LevelScene

--- TiaraForPrincess/Assets/Scripts/UI_Control.cs
+++ TiaraForPrincess/Assets/Scripts/UI_Control.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_Control : MonoBehaviour
 {
@@ -17,6 +18,11 @@ public class UI_Control : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 
     public void ViewBtnState(int[] arrCounts)
