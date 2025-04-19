de875bc90ea92fa37c12d9c8988826db67212408
alex23job <106354657+alex23job@users.noreply.github.com>
2025-04-20 01:40:13 +0300

    Adding Inventory panel control

---
+++ TiaraForPrincess/Assets/Scripts/InventPanelControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventPanelControl : MonoBehaviour
+{
+    [SerializeField] private Button btnUp;
+    [SerializeField] private Button btnDown;
+    [SerializeField] private Button[] arrBtnItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnUpClick()
+    {
+
+    }
+
+    public void OnDownClick()
+    {
+
+    }
+
+    public void OnBtnItemClick(int num)
+    {
+
+    }
+}
